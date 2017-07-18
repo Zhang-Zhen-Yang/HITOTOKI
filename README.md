6c5a8fc10708104de4067a9d60ae889718e80bc4
zzy <1097088392@qq.com>
2017-07-18 23:56:49 +0800

    modify lineup page

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 时刻(Hitotoki)是日本的一家新的创意型文具品牌，由キングジムは于2017年创办，因为它可以提供一种生活乐趣，把创意和实用融合在一起，该品牌的设计理念是“时刻享受生活中的乐趣”。
 
 ![](./screenshot/home.png)
-
+![](./screenshot/lineup.png)
 ![](./screenshot/gallery.png)
--- pages/kitta/kitta.js
+++ pages/kitta/kitta.js
@@ -13,12 +13,69 @@ Page({
     ],
     swipeIndex:0,
     navActiveIndex:0,
+
+    //first
     kittaTTe:[
       { img:'http://www.kingjim.co.jp/sp/kitta/images/hero-image-01.png',opacity:0},
       { img:'http://www.kingjim.co.jp/sp/kitta/images/hero-image-02.png',opacity:0},
       { img:'http://www.kingjim.co.jp/sp/kitta/images/hero-image-03.png',opacity:0}
     ],
 
+    //second
+    lineup:{
+      baseBaseUrl:'http://www.kingjim.co.jp/sp/kitta/images/nav-item-',
+      baseDetailsUrl:'http://www.kingjim.co.jp/sp/kitta/images/item-',
+      base:[
+        { img: 'kit001.png', tag: 'プレーン',},
+        { img: 'kit002.png', tag: 'シマシマ' },
+        { img: 'kit003.png', tag: 'ドット' },
+        { img: 'kit004.png', tag: 'チェック' },
+        { img: 'kit005.png', tag: 'チェック' },
+        { img: 'kit006.png', tag: 'メッセージ' },
+        { img: 'kit007.png', tag: 'フラワー' },
+        { img: 'kit008.png', tag: 'アニマル' },
+        { img: 'kit009.png', tag: 'ウツワ' },
+        { img: 'kit010.png', tag: 'ファブリック' },
+        { img: 'kit011.png', tag: 'ユメ' },
+        { img: 'kit012.png', tag: 'アサゴハン' },
+      ],
+      baseNew:[
+        { img: 'kit013.png', tag: 'プレーン2' },
+        { img: 'kit014.png', tag: 'シマシマ2' },
+        { img: 'kit015.png', tag: 'ドット2' },
+        { img: 'kit016.png', tag: 'チェック2' },
+        { img: 'kit017.png', tag: 'フレーム2' },
+        { img: 'kit018.png', tag: 'ノルディック' },
+        { img: 'kit019.png', tag: 'キカガク' },
+
+        { img: 'kit020.png', tag: 'オーロラ' },
+        { img: 'kit021.png', tag: 'ワガラ' },
+        { img: 'kit022.png', tag: 'フラワー2' },
+        { img: 'kit023.png', tag: 'マボロシ' },
+        { img: 'kit024.png', tag: 'ヤマ' },
+        { img: 'kit025.png', tag: 'フルーツ' },
+        { img: 'kit026.png', tag: 'ネコ' },
+        { img: 'kit027.png', tag: 'フラワー3' },
+        { img: 'kit028.png', tag: 'ホウソウシ' },
+        { img: 'kit029.png', tag: 'ファーム' },
+        { img: 'kit030.png', tag: 'オデカケ' },
+      ],
+      slimBaseUrl:'http://www.kingjim.co.jp/sp/kitta/images/nav-item-',
+      slim:[
+        { img: 'kits001.png', tag: 'ミックス' },
+        { img: 'kits002.png', tag: 'ミックス2' },
+        { img: 'kits003.png', tag: 'ミックス3' },
+        { img: 'kits004.png', tag: 'フェスティバル' },
+        { img: 'kits005.png', tag: 'ライン' },
+        { img: 'kits006.png', tag: 'チロリアン' },
+        
+      ],
+      detailsImg: 'http://www.kingjim.co.jp/sp/kitta/images/item-kit001.png',
+      dismiss:'http://www.kingjim.co.jp/sp/kitta/images/btn-close.png',
+      show:false
+    },
+
+    //third
     //相册
     gallery:[
       { img: 'http://www.kingjim.co.jp/sp/kitta/images/gallery-01.jpg', msg:'旅の記録を貼りつけて。'},
@@ -123,5 +180,33 @@ Page({
     this.setData({
       kittaTTe: oData
     });
+  },
+  //
+  lineupDetailsDismiss(e){
+    let lineup = this.data.lineup;
+      lineup.show = false;
+    this.setData({
+      lineup
+    });
+  },
+  //
+  showDetails(e){
+    let {index,type} = e.currentTarget.dataset,
+        detailsImagUrl;
+    switch(type){
+      case 'base': detailsImagUrl = this.data.lineup.baseDetailsUrl + this.data.lineup.base[index].img;
+        break;
+      case 'baseNew': detailsImagUrl = this.data.lineup.baseDetailsUrl + this.data.lineup.baseNew[index].img;
+        break;
+      case 'slim': detailsImagUrl = this.data.lineup.baseDetailsUrl + this.data.lineup.slim[index].img;
+        break;
+    } 
+    console.log(detailsImagUrl);
+    let lineup = this.data.lineup;
+    lineup.detailsImg = detailsImagUrl;
+    lineup.show = true;
+    this.setData({
+      lineup: lineup
+    })
   }
 })
